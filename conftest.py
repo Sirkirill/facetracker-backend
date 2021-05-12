bf5f6c0ede010d5af8e62e8d2e11fd1c0a698bf0
Sirkirill <sirbarkalov@gmail.com>
2021-05-12 23:52:05 +0300

    add tests

--- conftest.py
+++ conftest.py
@@ -1,6 +1,82 @@
+from functools import wraps
+
 import pytest
+from decouple import config
+
+from companies.models import Company
+from profiles.models import User
+from profiles.usecases import LoginUser
+
+
+TEST_USERS = {
+    'superuser_test': {'is_superuser': True, 'company': 'FaceIn'},
+
+    'admin_test': {'is_admin': True, 'company': 'FaceIn'},
+    'security_test': {'is_security': True, 'company': 'FaceIn'},
+    'ordinary_test': {'company': 'FaceIn'},
+
+    'f1_admin_test': {'is_admin': True, 'company': 'FaceIn1_test'},
+    'f1_security_test': {'is_security': True, 'company': 'FaceIn1_test'},
+    'f1_ordinary_test': {'company': 'FaceIn1_test'},
+
+    'f2_admin_test': {'is_admin': True, 'company': 'FaceIn2_test'},
+    'f2_security_test': {'is_security': True, 'company': 'FaceIn2_test'},
+    'f2_ordinary_test': {'company': 'FaceIn2_test'}
+}
 
 
 @pytest.fixture(scope='session')
 def django_db_setup():
     pass
+
+
+@pytest.fixture(scope='session', autouse=True)
+def test_users(django_db_blocker):
+    """
+    Fixture which creates users which will be used for testing before running tests
+    if these users don't exist.
+    """
+    with django_db_blocker.unblock():
+        for username, user_data in TEST_USERS.items():
+            company, _ = Company.objects.get_or_create(name=user_data.pop('company'))
+            user_data['company_id'] = company.id
+
+            try:
+                same_existed_user = User.objects.get(username=username, **user_data)
+                if not same_existed_user.check_password(config('TEST_PASSWORD')):
+                    raise Exception('Another user with this username already exists.')
+            except User.DoesNotExist:
+                user = User.objects.create(username=username, **user_data)
+                user.set_password(config('TEST_PASSWORD'))
+                user.save()
+
+
+def multiuser_test(login=True, **users_with_expected_results):
+    """
+    Decorator which helps to run test for different users. User and expected results
+        are added to function scope as user and expected variables.
+
+    Attributes:
+        users_with_expected_results (dict): Works as kwargs. Key is username of user and value is
+            a result which is expected from the test.
+
+    Example:
+        @multiuser_test(superuser_test=expected, username2=expected2, ...)
+        def test():
+            print(user, expected)
+
+    """
+
+    def test(func):
+        @wraps(func)
+        def run_tests(*args, **kwargs):
+            for username, expected in users_with_expected_results.items():
+                func.__globals__.update({'username': username, 'expected': expected})
+                if login:
+                    token = LoginUser(username, config('TEST_PASSWORD')).execute()['token']
+                    func.__globals__['token'] = token
+
+                return func(*args, **kwargs)
+
+        return run_tests
+    return test
--- facein_api/profiles/tests.py
+++ facein_api/profiles/tests.py
@@ -1,9 +1,14 @@
 import pytest
+from decouple import config
+from django.contrib.auth import get_user_model
 from django.urls import reverse
+
+from conftest import multiuser_test
+User = get_user_model()
 
 
 class TestLogin:
-    url = reverse('login')
+    url = reverse('profiles:login')
 
     @pytest.mark.django_db
     def test_login_without_data(self, client):
@@ -11,26 +16,78 @@ class TestLogin:
         assert response.status_code == 400, 'Not authenticated request returns 400 HTTP CODE'
 
     @pytest.mark.django_db
+    @multiuser_test(login=False, superuser_test=None, admin_test=None, security_test=None,
+                    ordinary_test=None)
     def test_login(self, client):
-        response = client.post(TestLogin.url, data={'username': 'gda2048', 'password': 'algolearn'})
-        assert response.status_code == 200, 'User gda2048 with password algolearn is not' \
-                                            'authenticated'
+        response = client.post(TestLogin.url, data={'username': username,
+                                                    'password': config('TEST_PASSWORD')})
+        assert response.status_code == 200, "User is authenticated"
 
     @pytest.mark.django_db
+    @multiuser_test(login=False, not_existed_test=None)
     def test_login_wrong_credentials(self, client):
-        response = client.post(TestLogin.url, data={'username': 'gda248', 'password': 'algolearn'})
-        assert response.status_code == 401, 'User doesn\'t exist'
+        response = client.post(TestLogin.url, data={'username': username,
+                                                    'password': config('TEST_PASSWORD')})
+        assert response.status_code == 401, "User does not exist"
 
 
 class TestLogout:
-    url = reverse('logout')
+    url = reverse('profiles:logout')
 
     @pytest.mark.django_db
+    @multiuser_test(superuser_test=None, admin_test=None, security_test=None,
+                    ordinary_test=None)
     def test_logout_after_login(self, client):
-        response = client.post(TestLogin.url, data={'username': 'gda2048', 'password': 'algolearn'})
-        token = response.json()['token']
+        """
+        Check that user can login and logout, and auth token is deleted after logout.
+        """
         response = client.get(TestLogout.url, HTTP_AUTHORIZATION='Token ' + token)
-        assert response.status_code == 204, 'User is succesfully unauthorized, token works'
+        assert response.status_code == 204, f"{username} is successfully unauthorized"
 
         response = client.get(TestLogout.url, HTTP_AUTHORIZATION='Token ' + token)
-        assert response.status_code == 401, 'User is already logout, token doesn\'t work'
+        assert response.status_code == 401, f"{username} can't login, token doesn't exist"
+
+
+class TestProfile:
+    url = reverse('profiles:profile')
+
+    @pytest.mark.django_db
+    @multiuser_test(superuser_test=None, admin_test=None, security_test=None,
+                    ordinary_test=None)
+    def test_get_my_profile(self, client):
+        response = client.get(TestProfile.url, HTTP_AUTHORIZATION='Token ' + token)
+        assert response.status_code == 200, f"{username} is successfully received"
+
+    @pytest.mark.django_db
+    @multiuser_test(superuser_test=None, admin_test=None, security_test=None,
+                    ordinary_test=None)
+    def test_update_my_profile(self, client):
+        response = client.put(TestProfile.url,
+                              HTTP_AUTHORIZATION='Token ' + token,
+                              data={'info': token},
+                              content_type='application/json')
+
+        assert response.status_code == 200, f"{username} is successfully updates"
+
+
+class TestStaff:
+
+    @pytest.mark.django_db
+    @multiuser_test(superuser_test=200, f1_ordinary_test=200, f2_admin_test=403)
+    def test_get_user_profile(self, client):
+        user = User.objects.get(username='f1_admin_test')
+        url = reverse('profiles:staff-detail', args=(user.id, ))
+        response = client.get(url, HTTP_AUTHORIZATION='Token ' + token)
+        assert response.status_code == expected, f'{username}'
+
+    @pytest.mark.django_db
+    @multiuser_test(superuser_test=200, f1_security_test=403, f1_admin_test=200,
+                    f1_ordinary_test=403, f2_admin_test=403)
+    def test_update_user_profile(self, client):
+        user = User.objects.get(username='f1_ordinary_test')
+        url = reverse('profiles:staff-detail', args=(user.id,))
+        response = client.put(url,
+                              HTTP_AUTHORIZATION='Token ' + token,
+                              data={'info': token},
+                              content_type='application/json')
+        assert response.status_code == expected, f'{username}'
